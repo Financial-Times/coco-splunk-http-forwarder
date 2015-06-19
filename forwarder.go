2910de8fa7111b5226d9e19e58c40921f72b4a72
Yolande Leung <yolande.leung@ft.com>
2015-06-19 17:03:59 +0100

    first cut of forwarder

---
+++ forwarder.go
@@ -0,0 +1,46 @@
+package main
+
+import(
+	"os"
+	"bufio"
+	"log"
+	"io"
+	"net/http"
+	"strings"
+	"crypto/tls"
+
+)
+
+
+func main() {
+	br := bufio.NewReader(os.Stdin)
+	for {
+		str, err := br.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			log.Fatal(err)
+		}
+		postToSplunk(str)
+	}
+}
+
+func postToSplunk(s string){
+	r, err := client.Post("https://log:5punk@splunk.glb.ft.com/coco-up/fleet", "application/json", strings.NewReader(s))
+	if err != nil {
+		log.Println(err)
+	} else {
+		if r.StatusCode != 200 {
+			log.Printf("Unexpected status code %v" , r.StatusCode)
+		}
+	}
+
+}
+
+var client *http.Client
+func init(){
+	tlsConfig := &tls.Config{InsecureSkipVerify: true}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client = &http.Client{Transport: transport}
+}
