565b4f319d20113357adb056371c4b18e780e3fc
Panziera, Luca <luca.panziera@ft.com>
2016-09-06 16:27:47 +0100

    Fixed typo in logging message

--- forwarder.go
+++ forwarder.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	log.Println("Splunk forwarder: Started")
-	defer log.Println("Splunk forwarder: Started")
+	defer log.Println("Splunk forwarder: Stopped")
 
 	br := bufio.NewReader(os.Stdin)
 	for {
